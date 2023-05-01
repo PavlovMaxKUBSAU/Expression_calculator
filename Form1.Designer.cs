bf68eba38be96ddcbc001ec6191010e681ebc217
PavlovMaxKUBSAU <80772614+PavlovMaxKUBSAU@users.noreply.github.com>
2023-05-01 21:35:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Exp_Calculator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calc_button = new System.Windows.Forms.Button();
+            this.X_label = new System.Windows.Forms.Label();
+            this.exp_label = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.reply_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Calc_button
+            // 
+            this.Calc_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.Calc_button.Location = new System.Drawing.Point(230, 143);
+            this.Calc_button.Name = "Calc_button";
+            this.Calc_button.Size = new System.Drawing.Size(160, 32);
+            this.Calc_button.TabIndex = 0;
+            this.Calc_button.Text = "Вычислить";
+            this.Calc_button.UseVisualStyleBackColor = true;
+            this.Calc_button.Click += new System.EventHandler(this.Calc_button_Click);
+            // 
+            // X_label
+            // 
+            this.X_label.AutoSize = true;
+            this.X_label.Font = new System.Drawing.Font("Footlight MT Light", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(209)));
+            this.X_label.Location = new System.Drawing.Point(30, 49);
+            this.X_label.Name = "X_label";
+            this.X_label.Size = new System.Drawing.Size(58, 15);
+            this.X_label.TabIndex = 1;
+            this.X_label.Text = "Число X:";
+            // 
+            // exp_label
+            // 
+            this.exp_label.AutoSize = true;
+            this.exp_label.Font = new System.Drawing.Font("Footlight MT Light", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(209)));
+            this.exp_label.Location = new System.Drawing.Point(12, 100);
+            this.exp_label.Name = "exp_label";
+            this.exp_label.Size = new System.Drawing.Size(79, 15);
+            this.exp_label.TabIndex = 2;
+            this.exp_label.Text = "Выражение:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.richTextBox1.Location = new System.Drawing.Point(94, 47);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(160, 25);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.richTextBox2.Location = new System.Drawing.Point(94, 96);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(500, 25);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox3.Enabled = false;
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.richTextBox3.Location = new System.Drawing.Point(94, 208);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(160, 25);
+            this.richTextBox3.TabIndex = 7;
+            this.richTextBox3.Text = "";
+            // 
+            // reply_label
+            // 
+            this.reply_label.AutoSize = true;
+            this.reply_label.Font = new System.Drawing.Font("Footlight MT Light", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(209)));
+            this.reply_label.Location = new System.Drawing.Point(45, 211);
+            this.reply_label.Name = "reply_label";
+            this.reply_label.Size = new System.Drawing.Size(43, 15);
+            this.reply_label.TabIndex = 6;
+            this.reply_label.Text = "Ответ:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 268);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.reply_label);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.exp_label);
+            this.Controls.Add(this.X_label);
+            this.Controls.Add(this.Calc_button);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Калькулятор выражений";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calc_button;
+        private System.Windows.Forms.Label X_label;
+        private System.Windows.Forms.Label exp_label;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label reply_label;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exp_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Calc_button_Click(object sender, EventArgs e)
+        {
+            string x = richTextBox1.Text;
+            if (x != "" && !double.TryParse(x, out double res))
+                MessageBox.Show("Значение X должно содержать только 1 число!\nДля ввода дробного используйте запятую", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            
+            string expression = richTextBox2.Text.Replace("X", x);
+            try
+            {
+                richTextBox3.Text = Solve_brackets(expression);
+            }
+            catch
+            {
+                MessageBox.Show("Неправильно введено выражение!\nДля ввода дробного используйте запятую", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        string Solve_brackets(string str)
+        {
+            if (str[0] == 42 || str[0] == 43 || str[0] == 47 || str[0] == 94
+                || str[str.Length - 1] == 42 || str[str.Length - 1] == 43 || str[str.Length - 1] == 45 || str[str.Length - 1] == 47 || str[str.Length - 1] == 94) // символы   * + - / ^        (в начале может быть знак -)
+            {
+                throw new InvalidOperationException();
+            }
+
+        start1:
+            bool brackets = false;
+            int br_counter = 0;
+
+            int left_i = 0, right_i = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(')
+                {
+                    brackets = true;
+                    br_counter++;
+
+                    if (br_counter == 1)
+                        left_i = i;
+                }
+                else if (str[i] == ')')
+                {
+                    if (brackets)
+                    {
+                        br_counter--;
+                        if (br_counter == 0)
+                        {
+                            right_i = i;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }
+
+
+
+
+            if (brackets)
+            {
+                if (!Check_Right(str, right_i))
+                {
+                    throw new InvalidOperationException();
+                }
+
+                if (Check_Left(str, left_i))
+                {
+                    str = Replace_str(str, left_i, right_i, Solve_brackets(str.Substring(left_i + 1, right_i - left_i - 1))).Replace("--", "+");
+                    if (str[0] == '+')
+                        str = str.Substring(1);
+                }
+                else
+                {
+                    left_i -= 3;
+                    if (str[left_i] == 's' && str[left_i + 1] == 'i' && str[left_i + 2] == 'n' && left_i >= 0 && Check_Left(str, left_i))
+                    {
+                        str = Replace_str(str, left_i, right_i, Math.Sin(Convert.ToDouble(Solve_brackets(str.Substring(left_i + 4, right_i - left_i - 4)))).ToString()).Replace("--", "+");
+                        if (str[0] == '+')
+                            str = str.Substring(1);
+                    }
+                    else if (str[left_i] == 'c' && str[left_i + 1] == 'o' && str[left_i + 2] == 's' && left_i >= 0 && Check_Left(str, left_i))
+                    {
+                        str = Replace_str(str, left_i, right_i, Math.Cos(Convert.ToDouble(Solve_brackets(str.Substring(left_i + 4, right_i - left_i - 4)))).ToString()).Replace("--", "+");
+                        if (str[0] == '+')
+                            str = str.Substring(1);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+
+                goto start1;
+            }
+
+
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != '0' && str[i] != '1' && str[i] != '2' && str[i] != '3' && str[i] != '4' && str[i] != '5' && str[i] != '6' && str[i] != '7' && str[i] != '8' && str[i] != '9' && str[i] != ',' && str[i] != '^' && str[i] != '*'
+                    && str[i] != '/' && str[i] != '+' && str[i] != '-')
+                    throw new InvalidOperationException();
+            }
+			
+			
+			
+			
+			str = str.Replace("--", "+");
+			if (str[0] == '+')
+				str = str.Substring(1);
+			
+        start2:
+            left_i = 0; right_i = 0;
+            int left_i2 = 0, right_i2 = 0;
+
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] == '^')
+                {
+                    right_i = i - 1;
+                    left_i = right_i;
+                    while (left_i >= 0 && (str[left_i] == '0' || str[left_i] == '1' || str[left_i] == '2' || str[left_i] == '3' || str[left_i] == '4' || str[left_i] == '5' || str[left_i] == '6' || str[left_i] == '7'
+                        || str[left_i] == '8' || str[left_i] == '9' || str[left_i] == ','))
+                    {
+                        left_i--;
+                    }
+
+                    if (left_i < 0 || str[left_i] != '-' || !Check_Left(str, left_i))
+                        left_i++;
+
+                    
+
+
+                    left_i2 = i + 1;
+                    right_i2 = left_i2;
+                    while (right_i2 < str.Length && (str[right_i2] == '0' || str[right_i2] == '1' || str[right_i2] == '2' || str[right_i2] == '3' || str[right_i2] == '4' || str[right_i2] == '5' || str[right_i2] == '6' || str[right_i2] == '7'
+                        || str[right_i2] == '8' || str[right_i2] == '9' || str[right_i2] == ',' || (str[right_i2] == '-' && right_i2 == left_i2)))
+                    {
+                        right_i2++;
+                    }
+                    right_i2--;
+
+
+
+                    str = Replace_str(str, left_i, right_i2, Calc(Convert.ToDouble(str.Substring(left_i, right_i - left_i + 1)), Convert.ToDouble(str.Substring(left_i2, right_i2 - left_i2 + 1)), str[i])).Replace("--", "+");
+                    if (str[0] == '+')
+                        str = str.Substring(1);
+
+                    goto start2;
+                }
+            }
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] == '*' || str[i] == '/')
+                {
+                    right_i = i - 1;
+                    left_i = right_i;
+                    while (left_i >= 0 && (str[left_i] == '0' || str[left_i] == '1' || str[left_i] == '2' || str[left_i] == '3' || str[left_i] == '4' || str[left_i] == '5' || str[left_i] == '6' || str[left_i] == '7'
+                        || str[left_i] == '8' || str[left_i] == '9' || str[left_i] == ','))
+                    {
+                        left_i--;
+                    }
+
+                    if (left_i < 0 || str[left_i] != '-' || !Check_Left(str, left_i))
+                        left_i++;
+
+
+
+
+                    left_i2 = i + 1;
+                    right_i2 = left_i2;
+                    while (right_i2 < str.Length && (str[right_i2] == '0' || str[right_i2] == '1' || str[right_i2] == '2' || str[right_i2] == '3' || str[right_i2] == '4' || str[right_i2] == '5' || str[right_i2] == '6' || str[right_i2] == '7'
+                        || str[right_i2] == '8' || str[right_i2] == '9' || str[right_i2] == ',' || (str[right_i2] == '-' && right_i2 == left_i2)))
+                    {
+                        right_i2++;
+                    }
+                    right_i2--;
+
+
+
+                    str = Replace_str(str, left_i, right_i2, Calc(Convert.ToDouble(str.Substring(left_i, right_i - left_i + 1)), Convert.ToDouble(str.Substring(left_i2, right_i2 - left_i2 + 1)), str[i])).Replace("--", "+");
+                    if (str[0] == '+')
+                        str = str.Substring(1);
+
+
+                    goto start2;
+                }
+            }
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] == '+')
+                {
+                    right_i = i - 1;
+                    left_i = 0;
+
+
+                    left_i2 = i + 1;
+                    right_i2 = left_i2;
+                    while (right_i2 < str.Length && (str[right_i2] == '0' || str[right_i2] == '1' || str[right_i2] == '2' || str[right_i2] == '3' || str[right_i2] == '4' || str[right_i2] == '5' || str[right_i2] == '6' || str[right_i2] == '7'
+                        || str[right_i2] == '8' || str[right_i2] == '9' || str[right_i2] == ',' || (str[right_i2] == '-' && right_i2 == left_i2)))
+                    {
+                        right_i2++;
+                    }
+                    right_i2--;
+					
+					
+					str = Replace_str(str, left_i, right_i2, Calc(Convert.ToDouble(str.Substring(left_i, right_i - left_i + 1)), Convert.ToDouble(str.Substring(left_i2, right_i2 - left_i2 + 1)), str[i]));
+
+                    goto start2;
+                }
+				
+				if (str[i] == '-')
+                {
+                    right_i = i - 1;
+                    left_i = 0;
+
+
+                    left_i2 = i + 1;
+                    right_i2 = left_i2;
+                    while (right_i2 < str.Length && (str[right_i2] == '0' || str[right_i2] == '1' || str[right_i2] == '2' || str[right_i2] == '3' || str[right_i2] == '4' || str[right_i2] == '5' || str[right_i2] == '6' || str[right_i2] == '7'
+                        || str[right_i2] == '8' || str[right_i2] == '9' || str[right_i2] == ',' || (str[right_i2] == '-' && right_i2 == left_i2)))
+                    {
+                        right_i2++;
+                    }
+                    right_i2--;
+					
+					
+					str = Replace_str(str, left_i, right_i2, Calc(Convert.ToDouble(str.Substring(left_i, right_i - left_i + 1)), Convert.ToDouble(str.Substring(left_i2, right_i2 - left_i2 + 1)), str[i]));
+
+                    goto start2;
+                }
+            }
+
+
+            return str;
+        }
+
+
+        bool Check_Left(string str, int i) =>
+            i == 0 || str[i - 1] == 40 || str[i - 1] == 42 || str[i - 1] == 43 || str[i - 1] == 45 || str[i - 1] == 47 || str[i - 1] == 94; // true, если это первый символ или слева один из следующих: ( * + - / ^
+
+        bool Check_Right(string str, int i) =>
+            i == str.Length-1 || str[i + 1] == 41 || str[i + 1] == 42 || str[i + 1] == 43 || str[i + 1] == 45 || str[i + 1] == 47 || str[i + 1] == 94; // true, если это последний символ или справа один из следующих: ) * + - / ^
+
+        string Replace_str(string str, int left_i, int right_i, string new_str) =>
+            str.Substring(0, left_i) + new_str + str.Substring(right_i + 1); //заменить все, что между left_i и right_i в str (включая left_i и right_i) на new_str
+
+
+        string Calc(double left, double right, char symb)
+        {
+            switch (symb)
+            {
+                case '^': return Math.Pow(left, right).ToString();
+                case '*': return (left * right).ToString();
+                case '/': return (left*1.0 / right).ToString();
+                case '+': return (left + right).ToString();
+                case '-': return (left - right).ToString();
+
+                default: throw new InvalidOperationException();
+            }
+        }
+    }
+}
